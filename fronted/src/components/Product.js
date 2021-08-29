dfbbf8260d1bd5b76823141f6942f30af1493ae2
hadi <hadi_960@hotmail.com>
2021-08-28 22:22:10 -0500

    adds product and home screen compononet

--- fronted/src/components/Product.js
+++ fronted/src/components/Product.js
@@ -1,3 +1,4 @@
+  
 import React from 'react'
 import Rating from './Rating';
 
@@ -5,11 +6,11 @@ export default function Product(props) {
     const { product } = props;
     return (
         <div key={product.id} className="card">
-        <a href={`/prduct/${product._id}`}>
+        <a href={`/product/${product._id}`}>
           <img className="medium" src={product.image} alt={product.name} />
         </a>
         <div className="card-body">
-          <a href={`/prduct/${product._id}`}>
+          <a href={`/product/${product._id}`}>
             <h2>{product.name}</h2>
           </a>
           <Rating 
