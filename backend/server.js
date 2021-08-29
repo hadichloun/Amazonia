8c85e4fa0ca61543668c96af489dce7961770a01
hadi <hadi_960@hotmail.com>
2021-08-29 17:32:36 -0500

    fetch catch err and adds err 500 when backend is down and style the box

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ import data from './data.js';
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products)
-})
+  res.send(data.products);
+});
 
 app.get('/', (req, res) => {
-    res.send('Server is ready');
-}); 
-const port = process.env.PORT || 5000   
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`)
-})
+  console.log(`Serve at http://localhost:${port}`);
+});
