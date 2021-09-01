361e85b66ce635e9a9d2cbc9cb9342c139b26a6c
hadi <hadi_960@hotmail.com>
2021-09-01 10:42:27 -0500

    connected mongDB and test data on the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
+
+//  .then(() => console.log('MongoDB Connected...')).catch(
+//   err => console.log(err))
+
+
+
+
+
 import express from 'express';
 import mongoose from 'mongoose';
 import data from './data.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazonia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  // useCreateIndex: true,
 });
 
 app.get('/api/products/:id', (req, res) => {
@@ -27,7 +35,7 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.status(500).send({ message: err.message });
 });
 
